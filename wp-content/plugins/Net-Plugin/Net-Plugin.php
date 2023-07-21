26a137428f83bf03a016b27130842207e5160189
0xAckerMan <joelkores1@gmail.com>
2023-07-21 23:10:25 +0300

    Initial commit

---
+++ wp-content/plugins/Net-Plugin/Net-Plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Net Plugin
+ * Plugin URI: http://www...
+ * Description: This is a plugin for my awesome project I am working on.
+ * Version: 1.0.0
+ * Author: Joel Kores
+ * Author URI: http://www...
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: net-plugin
+ */
+
+// security check
+defined('ABSPATH') or die('Go away Hacker!');
+
+
+
+?>
